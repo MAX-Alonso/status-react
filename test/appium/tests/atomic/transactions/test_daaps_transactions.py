c468a4b3aafa1442e81fadb3328ed28d477bc5f0
Churikova Tetiana <churikova.tm@gmail.com>
2020-03-25 14:06:03 +0100

    disabled tests with e2e-blockers

--- test/appium/tests/atomic/transactions/test_daaps_transactions.py
+++ test/appium/tests/atomic/transactions/test_daaps_transactions.py
@@ -79,6 +79,7 @@ class TestTransactionDApp(SingleDeviceTestCase):
 
     @marks.testrail_id(5743)
     @marks.high
+    # TODO: blocked by 10203
     def test_send_two_transactions_in_batch_in_dapp(self):
         sender = transaction_senders['W']
         sign_in_view = SignInView(self.driver)
